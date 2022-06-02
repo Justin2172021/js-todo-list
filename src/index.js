3071ba639ccf1468b2bb7fbb6c3e1124c9e4d032
Justin2172021 <justin.busching@gmail.com>
2022-06-02 16:42:25 -0500

    Ported in the UI of the library app from prior and refactored it (modulated and SR for webpack)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import { createToDo } from './create-to-do.js';
 import { blankProjectLoad } from './blank-project-load.js';
-import { initialdomManip } from './initial-dom-manip.js'
+import { displayDefaultProject, displayTheForm, addItemToCheckList, clearForm } from './dom-manip.js'
+import './style.css';
+
 
 // Call blankProjectLoad on first land
 blankProjectLoad();
 
-// Call DOM Manipulation module to control UI
-initialdomManip();
+// Call DOM Manipulation module to load default project
+//displayDefaultProject();
 
-// TODO: click event module HERE for todo and project creations
+// Click events module
+let clickEventsModule = (function() {
+
+    // Click event for displaying the form
+    const addNewToDo = document.querySelector(".add-todo-button");
+    addNewToDo.addEventListener("click", displayTheForm);
+
+    // Click event for adding an item to the checklist on the form
+    const addToChecklist = document.querySelector(".add-to-checklist");
+    addToChecklist.addEventListener("click", addItemToCheckList);
+
+    // Click event to clear the form
+    const clearButton = document.querySelector(".reset-button");
+    clearButton.addEventListener("click", clearForm);
+    
+})();
 
 // Call create-to-do.js module file and apply some objects/properties (this will eventually be replaced by UI input)
 const myToDo = createToDo("Grocery Run", "Go get groceries", "6/15/2022", "Low", "Meat, Eggs, Milk");
